4e4ce594b83d9416dd13c0f1413fd19bf62d9610
Mati Mutz <matias.mutz@gmail.com>
2020-10-15 15:16:23 -0300

    Models de CalendarioFestivos, CostoFab y genera

---
+++ SupplyChain/Models/CalendarioFestivos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SupplyChain.Shared.Models
+{
+    [Table("CalendarioFestivos")]
+    public class CalendarioFestivos
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [ColumnaGridViewAtributo(Name = "Registro")]
+        public decimal Registro { get; set; } = 0;
+        [ColumnaGridViewAtributo(Name = "Guid")]
+        public int Guid { get; set; } = 0;
+        [ColumnaGridViewAtributo(Name = "Fecha")]
+        public DateTime? Fecha { get; set; }
+        [ColumnaGridViewAtributo(Name = "Descripcion")]
+        public string Descripcion { get; set; } = "";
+    }
+}
