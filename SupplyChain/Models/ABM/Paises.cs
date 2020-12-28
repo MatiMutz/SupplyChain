1ad5a37145d792bdb2b9d9e9c0efef2590f8321c
Mati Mutz <matias.mutz@gmail.com>
2020-12-28 11:07:58 -0300

    28-12-20 Correccion de ABM. Andan: CateOperarios, Clases, Lineas, Operario, Prod, Scrap, TipoArea, TipoCelda, TiposNoConf. Genere vista de pedidos pendientes y quedo andando. Elimine los Models y Controllers que no servian. Cambie el nombre de los archivos y organice en carpetas para que quede mas organizado todo.

---
+++ SupplyChain/Models/ABM/Paises.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SupplyChain
+{
+    [Table("Pais")]
+    public class Pais
+    {
+        [ColumnaGridViewAtributo (Name = "Codigo")]
+        public int Codigo { get; set; } = 0;
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [ColumnaGridViewAtributo(Name = "Código de País")]
+        public int Cg_Pais { get; set; } = 0;
+        [ColumnaGridViewAtributo(Name = "País")]
+        public string Des_Pais { get; set; } = "";
+        [ColumnaGridViewAtributo(Name = "Mercosur")]
+        public bool Mercosur { get; set; } = false;
+        [ColumnaGridViewAtributo(Name = "Cuit")]
+        public string Cuit { get; set; } = "";
+    }
+}
+
+
+
+
+      
